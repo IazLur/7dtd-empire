ea6425b6a12398a1bdf6ee1ed89c0f73275c4685
Dragon Edouard <dragonedouard@hotmail.fr>
2020-04-23 01:04:58 +0200

    v0.0.2

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace EmpireLauncher7DTD
+{
+    partial class LauncherEmpire
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LauncherEmpire));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dossier = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.AddExtension = false;
+            this.openFileDialog1.CheckFileExists = false;
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "Sélectionner le répertoire d\'installation de 7 DTD";
+            // 
+            // dossier
+            // 
+            this.dossier.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("DejaVu Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(372, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(518, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "L\'Empire du Charcuteur";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(513, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 77);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Télécharger";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1265, 682);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(513, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 77);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Jouer";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(513, 412);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 77);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Quitter";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // LauncherEmpire
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "LauncherEmpire";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher Empire";
+            this.Load += new System.EventHandler(this.LauncherEmpire_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog dossier;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Diagnostics;
+
+namespace EmpireLauncher7DTD
+{
+    public partial class LauncherEmpire : Form
+    {
+        public string path = "";
+        public FtpWebRequest ftpRequest;
+        public FtpWebResponse ftpResponse;
+        public string ftpResponseList;
+        public Stream responseStream;
+        public StreamReader responseStreamReader;
+
+        public LauncherEmpire()
+        {
+            InitializeComponent();
+        }
+
+        private void LauncherEmpire_Load(object sender, EventArgs e)
+        {
+            label1.Parent = pictureBox1;
+            button1.Parent = pictureBox1;
+            button2.Parent = pictureBox1;
+            button3.Parent = pictureBox1;
+            label1.BackColor = Color.Transparent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Veuillez sélectionner le dossier d'installation de 7 days to die.", "Important", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            dossier.ShowDialog();
+
+            path = dossier.SelectedPath;
+
+            if (path.Contains("common")) {
+
+                if (!Directory.Exists(path + "\\mods"))
+                {
+                    Directory.CreateDirectory(path + "\\mods");
+                }
+
+                path = dossier.SelectedPath + "\\mods\\";
+
+                path = @Path.GetFullPath(path);
+                if (Directory.Exists(path + "..\\mods"))
+                {
+                    Directory.Delete(path + "..\\mods", true);
+                }
+                Directory.CreateDirectory(path + "..\\mods");
+
+                ftpRequest = (FtpWebRequest)WebRequest.Create("ftp://176.165.0.32/mods.zip");
+                ftpRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                ftpRequest.Credentials = new NetworkCredential("mods", "charcuterie");
+
+                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+
+                Stream responseStream = ftpResponse.GetResponseStream();
+                StreamReader responseStreamReader = new StreamReader(responseStream);
+
+                using (var outputFile = File.Create(path + "mods.zip"))
+                {
+                    responseStream.CopyTo(outputFile);
+                }
+
+                ZipFile.ExtractToDirectory(path + "mods.zip", path);
+
+                File.Delete(path + "mods.zip");
+
+                responseStream.Close();
+                responseStreamReader.Close();
+
+                ftpResponseList = File.ReadAllText(path + "mdp.txt");
+
+                MessageBox.Show("Le mot de passe pour se connecter au serveur : " + ftpResponseList, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else {
+                MessageBox.Show("Le jeu doit se trouver dans le répertoire de 'common' de Steam.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (dossier.SelectedPath != "")
+            {
+                Process.Start(path + "..\\" + "7DaysToDie_EAC.exe");
+            }
+            else
+            {
+                MessageBox.Show("Vous devez d'abord choisir dans quel dossier votre jeu est installé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
